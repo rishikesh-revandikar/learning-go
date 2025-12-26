e6fec13ff3bdb50616913ecc8a1ef0a123c4c437
Rishikesh Revandikar <rishikeshrevandikar3110@gmail.com>
2025-12-26 12:34:45 +0000

    Learned go-routines!!

---
+++ rough/goroutines/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func task(id int){
+	fmt.Println("Doing task ",id)
+}
+
+func main()  {
+	for i:=0; i <= 10; i++{
+		go task(i)
+	}
+	
+	time.Sleep(time.Second * 2)
+}
