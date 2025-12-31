bf2bbff7ca8ce2f4f64e6b028245aba158528c02
Rishikesh Revandikar <rishikeshrevandikar3110@gmail.com>
2025-12-31 15:26:08 +0000

    Completed api fetcher select version

--- 03_Concurrency/day18_api_fetcher/main.go
+++ 03_Concurrency/day18_api_fetcher/main.go
@@ -6,25 +6,53 @@ import (
 	"time"
 )
 
-func api_call(api int, wg *sync.WaitGroup){
-	defer func(){
+func api_call(api int, wg *sync.WaitGroup) {
+	defer func() {
 		wg.Done()
-		fmt.Println("API req completed :: ",api)
+		fmt.Println("API req completed :: ", api)
 	}()
-	fmt.Println("API req in processing :: ",api)
+	fmt.Println("API req in processing :: ", api)
 	time.Sleep(time.Second * 5)
+
+}
+
+func api_call_chan(req chan int, done chan bool) {
+	for {
+		select {
+		case api := <-req:
+			fmt.Println("API req in processing :: ", api)
+			time.Sleep(time.Second * 5)
+			fmt.Println("API req completed :: ", api)
+		default:
+			done<-true
+		}
+	}
 
 }
 
 func main() {
 
-	var wg sync.WaitGroup
+	/*
+	* Waitgroup implementation
+	 */
 
-	for i := 0; i < 3; i++{
-		wg.Add(1)
-		go api_call(i,&wg)
+	// var wg sync.WaitGroup
+
+	// for i := 0; i < 3; i++{
+	// 	wg.Add(1)
+	// 	go api_call(i,&wg)
+	// }
+	// wg.Wait()
+
+	requests := make(chan int, 10)
+	done := make(chan bool)
+
+	for i := 0; i < 5; i++ {
+		requests <- (i + 1)
 	}
-	wg.Wait()
 
+	go api_call_chan(requests,done)
+
+	<-done
 	fmt.Println("API calls completed!!")
 }
